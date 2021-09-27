b990682d1f9632250624b45240643d33433c3988
dzmitryNz <d.nayezzhy@gmail.com>
2021-09-27 13:53:49 +0300

    feat: other stations

--- bustime/index.js
+++ bustime/index.js
@@ -6,9 +6,11 @@ const timeNext = document.getElementById("timeNext");
 const timeNextKadino = document.getElementById("timeNextKadino");
 const timeNextKirova = document.getElementById("timeNextKirova");
 const timeNextRomanovichi = document.getElementById("timeNextRomanovichi");
+const timeNextVokzal = document.getElementById("timeNextVokzal");
 const sheduleKadino = document.getElementById("sheduleKadino");
 const sheduleKirova = document.getElementById("sheduleKirova");
 const sheduleRomanovichi = document.getElementById("sheduleRomanovichi");
+const sheduleVokzal = document.getElementById("sheduleVokzal");
 
 console.log(shedule)
 
@@ -32,17 +34,33 @@ const timeUpdate = () => {
 };
 
 const setShedule = () => {
-  let kirova, elArr, elNow;
+  let kirova, elArr, elNow, kadino, romanovichi, vokzal;
   if(day < 6) {
     kirova = shedule.kirova;
+    kadino = shedule.kadino;
+    romanovichi = shedule.romanovichi;
+    vokzal = shedule.vokzal;
   } else {
-    if(day === 6) console.log(day);
-    if(day === 7) console.log(day);
+    if(day === 6) {
+    kirova = shedule.kirova6;
+    kadino = shedule.kadino6;
+    romanovichi = shedule.romanovichi6;
+    vokzal = shedule.vokzal6;
+    }
+    if(day === 7) {
+      kirova = shedule.kirova7;
+      kadino = shedule.kadino7;
+      romanovichi = shedule.romanovichi7;
+      vokzal = shedule.vokzal7;
+    }
   };
   console.log(kirova)
   kirova = kirova.sort()
-    elNow = [];
-    elArr = [];
+  kadino = kadino.sort()
+  romanovichi = romanovichi.sort()
+  vokzal = vokzal.sort()
+
+  elNow = [];
   kirova.forEach(el => {
     const arrEl = el.split(":"); 
     const newEl = document.createElement("span");
@@ -61,6 +79,69 @@ const setShedule = () => {
     timeNextKirova.innerText = elNow.join(",  ");
     newEl.innerText = el;
     sheduleKirova.appendChild(newEl);
+  });
+
+  elNow = [];
+  kadino.map(el => {
+    const arrEl = el.split(":"); 
+    const newEl = document.createElement("span");
+    if(Number(arrEl[0]) >= hours) {
+      newEl.classList.add("shedule");
+      if(Number(arrEl[0]) === hours && Number(arrEl[1]) < mins) newEl.classList.add("oldshedule")
+      if(Number(arrEl[0]) === hours && Number(arrEl[1]) >= mins) {
+        newEl.classList.add("shedulenow");
+        elNow.push(el);
+      }
+      if((Number(arrEl[0]) - hours) > 1) newEl.classList.add("shedulelong");
+    } else {
+      newEl.classList.add("shedule")
+      newEl.classList.add("oldshedule")
+    }
+    timeNextKadino.innerText = elNow.join(",  ");
+    newEl.innerText = el;
+    sheduleKadino.appendChild(newEl);
+  });
+
+  elNow = [];
+  romanovichi.map(el => {
+    const arrEl = el.split(":"); 
+    const newEl = document.createElement("span");
+    if(Number(arrEl[0]) >= hours) {
+      newEl.classList.add("shedule");
+      if(Number(arrEl[0]) === hours && Number(arrEl[1]) < mins) newEl.classList.add("oldshedule")
+      if(Number(arrEl[0]) === hours && Number(arrEl[1]) >= mins) {
+        newEl.classList.add("shedulenow");
+        elNow.push(el);
+      }
+      if((Number(arrEl[0]) - hours) > 1) newEl.classList.add("shedulelong");
+    } else {
+      newEl.classList.add("shedule")
+      newEl.classList.add("oldshedule")
+    }
+    timeNextRomanovichi.innerText = elNow.join(",  ");
+    newEl.innerText = el;
+    sheduleRomanovichi.appendChild(newEl);
+  });
+
+  elNow = [];
+  vokzal.map(el => {
+    const arrEl = el.split(":"); 
+    const newEl = document.createElement("span");
+    if(Number(arrEl[0]) >= hours) {
+      newEl.classList.add("shedule");
+      if(Number(arrEl[0]) === hours && Number(arrEl[1]) < mins) newEl.classList.add("oldshedule")
+      if(Number(arrEl[0]) === hours && Number(arrEl[1]) >= mins) {
+        newEl.classList.add("shedulenow");
+        elNow.push(el);
+      }
+      if((Number(arrEl[0]) - hours) > 1) newEl.classList.add("shedulelong");
+    } else {
+      newEl.classList.add("shedule")
+      newEl.classList.add("oldshedule")
+    }
+    timeNextVokzal.innerText = elNow.join(",  ");
+    newEl.innerText = el;
+    sheduleVokzal.appendChild(newEl);
   });
 };
 
